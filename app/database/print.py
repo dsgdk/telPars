46707f71c2895cb36876b8888cfebf26115218d2
dsgdk <daniilsagaydak996@gmail.com>
2024-10-05 17:12:43 +0300

    tg bot

--- app/database/print.py
+++ app/database/print.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import sessionmaker
-from models import TelegramMessage, get_engine
+from .models import TelegramMessage, get_engine
 
 engine  = get_engine()
 Session = sessionmaker(bind=engine)
@@ -10,16 +10,31 @@ def get_messages():
     last_10_messages.sort(key=lambda message: message.id)
     
     # -- print 
-    for message in last_10_messages:
-        print(f"    id:           {message.id}")
-        print(f"    message_text: {message.message_text}")
-        print(f"    message_date: {message.message_date}")
-        print(f"    sender_id:    {message.sender_id}")
-        print(f"    first_name:   {message.first_name}")
-        print(f"    last_name:    {message.last_name}")
-        print(f"    username:     {message.username}")
-        print(f"    phone_number: {message.phone_number}")
-        print("-" * 65)
+    # for message in last_10_messages:
+    #     print(f"    id:           {message.id}")
+    #     print(f"    message_text: {message.message_text}")
+    #     print(f"    message_date: {message.message_date}")
+    #     print(f"    sender_id:    {message.sender_id}")
+    #     print(f"    first_name:   {message.first_name}")
+    #     print(f"    last_name:    {message.last_name}")
+    #     print(f"    username:     {message.username}")
+    #     print(f"    phone_number: {message.phone_number}")
+    #     print("-" * 65)
 
-get_messages()
-session.close()
+    # Форматування повідомлення
+    message_text = ""
+    for message in last_10_messages:
+        message_text += (
+            f"id: {message.id}\n"
+            f"message_text: {message.message_text}\n"
+            f"message_date: {message.message_date}\n"
+            f"sender_id: {message.sender_id}\n"
+            f"first_name: {message.first_name}\n"
+            f"last_name: {message.last_name}\n"
+            f"username: {message.username}\n"
+            f"phone_number: {message.phone_number}\n"
+            f"{'-' * 65}\n"
+        )
+
+    session.close()  # Закрийте сесію
+    return message_text
---
+++ app/tg_bot.py
@@ -0,0 +1,21 @@
+from dotenv         import load_dotenv
+from database.print import get_messages
+
+import telebot
+import os
+
+load_dotenv()
+token = os.getenv('BT_TOKEN')
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Hello!')
+
+@bot.message_handler(commands=['last_messages'])
+def send_last_messages(message):
+    last_messages = get_messages()  # Отримуємо останні повідомлення
+    bot.send_message(message.chat.id, last_messages)  # Надсилаємо їх у Telegram
+
+bot.polling(none_stop=True)
